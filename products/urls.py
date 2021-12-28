6af0e0dd718b0a1b0208dce4ba183f0eb5d57d55
seuljaa <seuljaa940503@gmail.com>
2021-12-28 13:25:27 +0900

    검색박스생성

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('list/', views.product_list),
+    path('list/', views.product_list, name='list'),
     path('list/<int:product_id>/', views.product_detail, name='detail'),
     path('list/<int:product_id>/question_create/',views.question_create,name='question_create'),
 ]
--- products/views.py
+++ products/views.py
@@ -6,23 +6,35 @@ from products.models import Product
 from questions.models import Question
 from django.core.paginator import Paginator
 from .forms import QuestionForm
+from django.db.models import Q
 # Create your views here.
 
 
 def product_list(request):
     page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')
     products = Product.objects.order_by('-reg_date')
+    if kw:
+        product_list = products.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) |  # 내용검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
     paginator = Paginator(products, 10)
     page_obj = paginator.get_page(page)
-    context = {'products':page_obj}
+    context = {'products':page_obj, 'page':page, 'kw':kw}
     return render(request, 'products/products_list.html', context)
 
 def product_detail(request, product_id):
     products = Product.objects.get(pk=product_id)
     product_reals = products.productreal_set.order_by('option_1_display_name', 'option_2_display_name')
     ct = ContentType.objects.get_for_model(products)
+    page = request.GET.get('page','1')
     questions = Question.objects.filter(content_type=ct,object_id=product_id)
-    return render(request, 'products/products_detail.html', {'product_reals':product_reals, 'products':products, 'questions':questions} )
+    paginator = Paginator(questions, 10)
+    page_obj = paginator.get_page(page)
+    return render(request, 'products/products_detail.html', {'product_reals':product_reals, 'products':products, 'questions':page_obj} )
 
 @login_required(login_url='accounts:signin')
 def question_create(request, product_id):
