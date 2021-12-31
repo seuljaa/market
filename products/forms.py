9cc0d854399eda6ef8d08b7f7444f4add63e470c
seuljaa <seuljaa940503@gmail.com>
2021-12-31 12:34:00 +0900

    장바구니 디자인변경

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from questions.models import Question
+from cart.models import CartItem
 
 
 class QuestionForm(forms.ModelForm):
@@ -8,4 +9,13 @@ class QuestionForm(forms.ModelForm):
         fields = ['body']
         labels = {
             'body': '내용',
+        }
+
+class CartForm(forms.ModelForm):
+    class Meta:
+        model = CartItem  # 사용할 모델
+        fields = ['product_real', 'quantity']
+        labels = {
+            'product_real': '내용',
+            'quantity' : '수량',
         }
