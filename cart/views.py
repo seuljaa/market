e0c7d021c6cc7a826b4bc15cacd77676cf4a9ed4
seuljaa <seuljaa940503@gmail.com>
2022-01-03 15:25:27 +0900

    선택한옵션이 장바구니에 추가됨

--- cart/views.py
+++ cart/views.py
@@ -1,15 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from products.models import ProductReal
 from .forms import CartForm
+from django.http import HttpResponse
 
 # Create your views here.
 
-
+@login_required(login_url='accounts:signin')
 def cart_add(request, product_id):
-    product_real_size=request.GET.get('size')
-    product_real_color=request.GET.get('color')
+    product_real_size = request.POST.get('size')
+    product_real_color = request.POST.get('color')
     for value in ProductReal.objects.all():
-        if value.id==53:
+        if value.product_id == product_id and value.option_1_name == product_real_size and value.option_2_name == product_real_color :
             index = value.id
     form = CartForm(request.POST)
     if form.is_valid():
