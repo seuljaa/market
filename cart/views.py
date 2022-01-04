17ff8637eab8aed0aa65608fc3c0230e7fa9d2ed
seuljaa <seuljaa940503@gmail.com>
2022-01-04 17:22:43 +0900

    장바구니에 상품을 담으면 다시 해당상품 상세페이지 출력

--- cart/views.py
+++ cart/views.py
@@ -15,13 +15,14 @@ def cart_add(request, product_id):
     for value in ProductReal.objects.all():
         if value.product_id == product_id and value.option_1_name == product_real_size and value.option_2_name == product_real_color :
             index = value.id
-    form = CartForm(request.POST)
-    if form.is_valid():
-        cart=form.save(commit=False)
-        cart.user_id=request.user.id
-        cart.product_real_id = index
-        cart.save()
-        return redirect('products:detail', product_id=9)
+            id = value.product_id
+            form = CartForm(request.POST)
+            if form.is_valid():
+                cart=form.save(commit=False)
+                cart.user_id=request.user.id
+                cart.product_real_id = index
+                cart.save()
+                return redirect('products:detail', product_id=id)
 
 @login_required(login_url='accounts:signin')
 def cart_list(request):
